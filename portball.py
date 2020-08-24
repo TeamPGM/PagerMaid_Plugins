5567412c9e54ba28ef6ab54174f333d0813204fa
Pentacene <48641773+Pentacene@users.noreply.github.com>
2020-08-24 10:05:50 +0800

    🎉 Portball 1.21 and atadmin_atall 1.1 (#12)

--- portball.py
+++ portball.py
@@ -6,9 +6,9 @@ from datetime import timedelta
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
-@listener(is_plugin=True, outgoing=True, command="portball",
-          description="回复你要临时禁言的人的消息来实现XX秒的禁言。",
-          parameters="<理由> <时间 单位：秒>")
+@listener(incoming=True, outgoing=True, command="portball",
+          description="回复你要临时禁言的人的消息来实现XX秒的禁言",
+          parameters="<理由>(空格)<时间/秒>")
 async def portball(context):
     reply = await context.get_reply_message()
     if context.is_group:
@@ -18,11 +18,8 @@ async def portball(context):
                 last_name = ''
             else:
                 last_name = reply.sender.last_name
-            if not len(action) == 2:
-                await context.edit('你好蠢诶，没有参数我怎么禁言嘛。')
-                return
             if int(action[1])<60:
-                notification = await bot.send_message(context.chat_id, '诶呀不要小于60秒啦')
+                notification = await bot.send_message(context.chat_id, '诶呀不要小于60秒啦', reply_to = context.id)
                 await sleep(10)
                 await notification.delete()
                 try:
@@ -33,31 +30,34 @@ async def portball(context):
             admins = await context.client.get_participants(context.chat, filter=ChannelParticipantsAdmins)
             if context.sender in admins:
                 try:
-                    await bot.edit_permissions(context.chat_id, reply.sender.id, timedelta(seconds=int(action[1].replace(' ',''))), send_messages=False)
-                    await bot.send_message(
+                    await bot.edit_permissions(context.chat_id, reply.sender.id, timedelta(seconds=int(action[1].replace(' ',''))), send_messages=False, send_media=False, send_stickers=False, send_gifs=False, send_games=False, send_inline=False, send_polls=False, invite_users=False, change_info=False, pin_messages=False)
+                    portball_message = await bot.send_message(
                         context.chat_id,
                         f'[{reply.sender.first_name}{last_name}](tg://user?id={reply.sender.id}) 由于 {action[0]} \n'
                         f'被暂时禁言{action[1]}秒',
                         reply_to = reply.id
                     )
+                    await context.delete()
+                    await sleep(int(action[1].replace(' ','')))
+                    await portball_message.delete()
                 except rpcerrorlist.UserAdminInvalidError:
-                    notification = await bot.send_message(context.chat_id, '错误：我没有管理员权限或我的权限比被封禁的人要小')
+                    notification = await bot.send_message(context.chat_id, '错误：我没有管理员权限或我的权限比被封禁的人要小', reply_to = context.id)
                     await sleep(10)
                     await notification.delete()
                 except rpcerrorlist.ChatAdminRequiredError:
-                    notification = await bot.send_message(context.chat_id, '错误：我没有管理员权限或我的权限比被封禁的人要小')
+                    notification = await bot.send_message(context.chat_id, '错误：我没有管理员权限或我的权限比被封禁的人要小', reply_to = context.id)
                     await sleep(10)
                     await notification.delete()
             else:
-                notification = await bot.send_message(context.chat_id, '诶呀你不是管理员，不要给人家塞口球啊')
+                notification = await bot.send_message(context.chat_id, '诶呀你不是管理员，不要给人家塞口球啊', reply_to = context.id)
                 await sleep(10)
                 await notification.delete()
         else:
-            notification = await bot.send_message(context.chat_id, '你好蠢诶，都没有回复人，我哪知道你要搞谁的事情……')
+            notification = await bot.send_message(context.chat_id, '你好蠢诶，都没有回复人，我哪知道你要搞谁的事情……', reply_to = context.id)
             await sleep(10)
             await notification.delete()
     else:
-        notification = await bot.send_message(context.chat_id, '你好蠢诶，又不是群组，怎么禁言啦！')
+        notification = await bot.send_message(context.chat_id, '你好蠢诶，又不是群组，怎么禁言啦！', reply_to = context.id)
         await sleep(10)
         await notification.delete()
     try:
