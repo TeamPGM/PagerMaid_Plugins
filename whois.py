e74f374d1cc109627527b8fbeda98b1fb9f1e0f4
Xtao_dada <xtao@xtaolink.cn>
2020-11-28 19:28:00 +0800

    📦 whois 优化逻辑 , 修改措辞 (#81)
    * 📦 whois 优化逻辑 , 修改措辞
    
    * Update list.json

--- whois.py
+++ whois.py
@@ -16,8 +16,8 @@ async def whois(context):
         return
     req = get("https://tenapi.cn/whois/?url=" + message)
     if req.status_code == 200:
-        data = json.loads(req.text)
-        res = '网址： ' + data['data']['url'] + '\n注册商： ' + str(data['data']['registrar']) + '\n联系人： ' + str(data['data']['registrant']) + '\n联系邮箱： ' + str(data['data']['mail']) + '\n注册时间： ' + str(data['data']['registration']) + '\n过期时间： ' + str(data['data']['expiration']) + '\nDNS： ' + str(data['data']['dns'])
+        data = json.loads(req.text)['data']
+        res = '域名： `' + data['url'] + '`\n注册商： `' + str(data['registrar']) + '`\n联系人： `' + str(data['registrant']) + '`\n联系邮箱： `' + str(data['mail']) + '`\n注册时间： `' + str(data['registration']) + '`\n过期时间： `' + str(data['expiration']) + '`\nDNS： ' + str(data['dns']).replace('<br/>', '\n')
         await context.edit(res)
     else:
         await context.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
