3070c5cf0d6ebe526ad44471553d5b54eb4ccc54
c3b2a <62082214+c3b2a@users.noreply.github.com>
2021-02-07 21:08:38 +0800

    添加 try 防止运行时错误

--- keyword_func/newkeyword.py
+++ keyword_func/newkeyword.py
@@ -1,4 +1,4 @@
- import json
+import json
 from os import remove, mkdir
 from os.path import isfile, exists
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -35,57 +35,60 @@ def init_file(filename):
 
 
 async def main(context):
-    chat_id = context.chat_id
-    if chat_id < 0:
-        admins = await context.client.get_participants(context.chat, filter=ChannelParticipantsAdmins)
-        text = context.text
-        if not text:
-            text = ""
-        if text.split()[0] == "/add":
-            if context.sender in admins:
-                try:
-                    parse = text.split("\n")
-                    parse[0] = " ".join(parse[0].split()[1:])
-                    init_file(f"newkeyword_{chat_id}.json")
-                    data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
-                    data[parse[0]] = parse[1]
-                    write_data(f"newkeyword_{chat_id}.json", json.dumps(data))
-                    await context.client.send_message(chat_id, "设置成功", reply_to=context.id)
-                except:
-                    await context.client.send_message(chat_id, "设置失败", reply_to=context.id)
+    try:
+        chat_id = context.chat_id
+        if chat_id < 0:
+            admins = await context.client.get_participants(context.chat, filter=ChannelParticipantsAdmins)
+            text = context.text
+            if not text:
+                text = ""
+            if text.split()[0] == "/add":
+                if context.sender in admins:
+                    try:
+                        parse = text.split("\n")
+                        parse[0] = " ".join(parse[0].split()[1:])
+                        init_file(f"newkeyword_{chat_id}.json")
+                        data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
+                        data[parse[0]] = parse[1]
+                        write_data(f"newkeyword_{chat_id}.json", json.dumps(data))
+                        await context.client.send_message(chat_id, "设置成功", reply_to=context.id)
+                    except:
+                        await context.client.send_message(chat_id, "设置失败", reply_to=context.id)
+                else:
+                    await context.client.send_message(chat_id, "您无权进行此操作", reply_to=context.id)
+            elif text.split()[0] == "/del":
+                if context.sender in admins:
+                    try:
+                        init_file(f"newkeyword_{chat_id}.json")
+                        data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
+                        del data[" ".join(text.split(" ")[1:])]
+                        write_data(f"newkeyword_{chat_id}.json", json.dumps(data))
+                        await context.client.send_message(chat_id, "删除成功", reply_to=context.id)
+                    except:
+                        await context.client.send_message(chat_id, "删除失败", reply_to=context.id)
+                else:
+                    await context.client.send_message(chat_id, "您无权进行此操作", reply_to=context.id)
+            elif text.split()[0] == "/list":
+                if context.sender in admins:
+                    try:
+                        init_file(f"newkeyword_{chat_id}.json")
+                        data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
+                        message = ""
+                        count = 1
+                        for k, v in data.items():
+                            message += f"`{count}` : `{k}` -> `{v}`\n"
+                            count += 1
+                        await context.client.send_message(context.sender_id, message)
+                        await context.client.send_message(chat_id, "已发送私聊", reply_to=context.id)
+                    except:
+                        await context.client.send_message(chat_id, "获取失败", reply_to=context.id)
+                else:
+                    await context.client.send_message(chat_id, "您无权进行此操作", reply_to=context.id)
             else:
-                await context.client.send_message(chat_id, "您无权进行此操作", reply_to=context.id)
-        elif text.split()[0] == "/del":
-            if context.sender in admins:
-                try:
-                    init_file(f"newkeyword_{chat_id}.json")
-                    data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
-                    del data[" ".join(text.split(" ")[1:])]
-                    write_data(f"newkeyword_{chat_id}.json", json.dumps(data))
-                    await context.client.send_message(chat_id, "删除成功", reply_to=context.id)
-                except:
-                    await context.client.send_message(chat_id, "删除失败", reply_to=context.id)
-            else:
-                await context.client.send_message(chat_id, "您无权进行此操作", reply_to=context.id)
-        elif text.split()[0] == "/list":
-            if context.sender in admins:
-                try:
-                    init_file(f"newkeyword_{chat_id}.json")
-                    data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
-                    message = ""
-                    count = 1
-                    for k, v in data.items():
-                        message += f"`{count}` : `{k}` -> `{v}`\n"
-                        count += 1
-                    await context.client.send_message(context.sender_id, message)
-                    await context.client.send_message(chat_id, "已发送私聊", reply_to=context.id)
-                except:
-                    await context.client.send_message(chat_id, "获取失败", reply_to=context.id)
-            else:
-                await context.client.send_message(chat_id, "您无权进行此操作", reply_to=context.id)
-        else:
-            data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
-            for k, v in data.items():
-                if k in text:
-                    await context.client.send_message(chat_id, v, reply_to=context.id)
-    return ""
+                data = json.loads(get_data(f"newkeyword_{chat_id}.json"))
+                for k, v in data.items():
+                    if k in text:
+                        await context.client.send_message(chat_id, v, reply_to=context.id)
+        return ""
+    except:
+        return ""
