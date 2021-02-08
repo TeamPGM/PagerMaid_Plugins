57944ec2e5205b8d5318d96daf0dd4eb1d6cef2f
xtaodada <xtao@xtaolink.cn>
2021-02-08 23:35:59 +0800

    👷 Beta 插件更新通知

---
+++ telegram_update_beta.py
@@ -0,0 +1,25 @@
+import json, time, sys
+from requests import get, post
+
+token = str(sys.argv[1])
+main = json.loads(get("https://api.github.com/repos/xtaodada/PagerMaid_Plugins/commits/beta").content)
+push_content = {}
+push_content['chat_id'] = '-1001441461877'
+push_content['disable_web_page_preview'] = 'True'
+push_content['parse_mode'] = 'markdown'
+push_content['text'] = "#更新日志 #beta #" + main['commit']['author'][
+            'name'].replace('_', '') + ' \n\n🔨 [' + main['sha'][0:7] + '](https://github.com/xtaodada/PagerMaid_Plugins/compare/' + \
+                               main[
+                                   'sha'] + '): ' + main['commit']['message']
+url = 'https://api.telegram.org/bot' + token + '/sendMessage'
+try:
+    main_req = post(url, data=push_content)
+except:
+    pass
+push_content['chat_id'] = '-1001319957857'
+time.sleep(1)
+try:
+    main_req = get(url, data=push_content)
+except:
+    pass
+print(main['sha'] + " ok！")
