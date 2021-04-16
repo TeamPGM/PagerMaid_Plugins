532d94adff73837f443bbdd62490f695237954c1
b miao <blucartfhg@outlook.com>
2021-04-16 23:44:46 +0800

    paolu v1.01

--- paolu.py
+++ paolu.py
@@ -7,19 +7,28 @@ from pagermaid.listener import listener
           description="⚠一键跑路 删除群内消息并禁言⚠")
 async def paolu(context):
     """一键跑路 删除群内消息并禁言"""
-    await bot.edit_permissions(
-                        entity=context.chat_id,
-                        send_messages=False,
-                        send_media=False,
-                        send_stickers=False,
-                        send_gifs=False,
-                        send_games=False,
-                        send_inline=False,
-                        send_polls=False,
-                        invite_users=False,
-                        change_info=False,
-                        pin_messages=False)
+    try:
+        await bot.edit_permissions(
+                            entity=context.chat_id,
+                            send_messages=False,
+                            send_media=False,
+                            send_stickers=False,
+                            send_gifs=False,
+                            send_games=False,
+                            send_inline=False,
+                            send_polls=False,
+                            invite_users=False,
+                            change_info=False,
+                            pin_messages=False)
+    except:
+        pass
     await bot.delete_messages(context.chat_id, list(range(1,context.message.id)))
+    try:
+        await bot.edit_permissions(
+                            entity=context.chat_id,
+                            send_messages=False)
+    except:
+        pass
     await context.edit("Finished")
     await sleep(10)
     await context.delete()
