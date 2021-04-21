416017056655a65cadf8dde6fcf118d8063a48ab
一只瓜 <52106595+a-fruity-melon@users.noreply.github.com>
2021-04-21 18:32:19 +0800

    🔥 feat: rate.py supports more currencies
    ... and get rid of api keys!

--- rate.py
+++ rate.py
@@ -1,75 +1,88 @@
-""" Pagermaid currency exchange rates plugin. Plugin by @fruitymelon and @xtaodada """
+""" Pagermaid currency exchange rates plugin. Plugin by @fruitymelon and @xtaodada"""
 
-import sys
+import asyncio, json, time
+from json.decoder import JSONDecodeError
 import urllib.request
 from pagermaid.listener import listener, config
+from pagermaid import log
 
 # i18n 
 ## 默认语言
-lang_rate = {"des": "货币汇率插件", "arg": "<FROM> <TO> <NUM>", "not": "请先安装依赖：`python3 -m pip install xmltodict`\n随后，请重启 pagermaid。", "help": "这是货币汇率插件\n\n使用方法: `-rate <FROM> <TO> <NB>`\n\n支持货币: \n", "nc": "不是支持的货币. \n\n支持货币: \n"}
+lang_rate = {"des": "货币汇率插件", "arg": "<FROM> <TO> <NUM>", "help": "这是货币汇率插件\n\n使用方法: `-rate <FROM> <TO> <NUM>，其中 <NUM> 是可省略的`\n\n支持货币: \n", "nc": "不是支持的货币. \n\n支持货币: \n", "notice": "数据每日更新，建议使用 bc 插件查看加密货币汇率", "warning": "数据每日更新"}
 ## 其他语言
 if config["application_language"] == "en":
-    lang_rate = {"des": "Currency exchange rate plugin", "arg": "<FROM> <TO> <NUM>", "not": "Please run: `python3 -m pip install xmltodict`\nand restart pagermaid。", "help": "Currency exchange rate plugin\n\nUsage: `-rate <FROM> <TO> <NB>`\n\nSupport: \n", "nc": "is not support\n\nSupport: \n"}
+    lang_rate = {"des": "Currency exchange rate plugin", "arg": "<FROM> <TO> <NUM>", "help": "Currency exchange rate plugin\n\nUsage: `-rate <FROM> <TO> <NUM> where <NUM> is optional`\n\nAvailable currencies: \n", "nc": "is not available.\n\nAvailable currencies: \n", "notice": "Data are updated daily, for encrypted currencies we recommend to use the `bc` plugin.", "warning": "Data are updated daily"}
 
-imported = True
-API = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+
+API = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
 currencies = []
-rates = {}
+data = {}
+
+inited = False
 
 
 def init():
-    with urllib.request.urlopen(API) as response:
-        result = response.read()
-        try:
-            global rate_data, rates
-            rate_data = xmltodict.parse(result)
-            rate_data = rate_data['gesmes:Envelope']['Cube']['Cube']['Cube']
-            for i in rate_data:
-                currencies.append(i['@currency'])
-                rates[i['@currency']] = float(i['@rate'])
-            currencies.sort()
-        except Exception as e:
-            raise e
+  with urllib.request.urlopen(API) as response:
+    result = response.read()
+    try:
+      global data
+      data = json.loads(result)
+      for key in list(enumerate(data)):
+        currencies.append(key[1].upper())
+      currencies.sort()
+    except JSONDecodeError as e:
+      raise e
+    global inited
+    inited = True
 
 
-try:
-    import xmltodict
+init()
+last_init = time.time()
 
-    init()
-except ImportError:
-    imported = False
-
-
-def logsync(message):
-    sys.stdout.writelines(f"{message}\n")
-
+@listener(incoming=True, ignore_edited=True)
+async def refresher(context):
+  if time.time() - last_init > 24 * 60 * 60:
+    # we'd better do this to prevent ruining the log file with massive fail logs
+    # as this `refresher` would be called frequently
+    last_init = time.time()
+    try:
+      init()
+    except Exception as e:
+      await log(f"Warning: plugin rate failed to refresh rates data. {e}")
 
 @listener(is_plugin=True, outgoing=True, command="rate",
           description=lang_rate["des"],
           parameters=lang_rate["arg"])
 async def rate(context):
-    if not imported:
-        await context.edit(lang_rate["not"])
-        return
-    if not context.parameter:
-        await context.edit(
-            f"{lang_rate['help']}{', '.join(currencies)}")
-        return
-    if len(context.parameter) != 3:
-        await context.edit(f"{lang_rate['help']}{', '.join(currencies)}")
-        return
-    FROM = context.parameter[0].upper().strip()
-    TO = context.parameter[1].upper().strip()
+  if not inited:
+    init()
+  if not inited:
+    return
+  if not context.parameter:
+    await context.edit(f"{lang_rate['help']}`{', '.join(currencies)}`\n\n{lang_rate['notice']}")
+    return
+  NB = 1.0
+  if len(context.parameter) != 3:
+    if len(context.parameter) != 2:
+      await context.edit(f"{lang_rate['help']}`{', '.join(currencies)}`\n\n{lang_rate['notice']}")
+      return
+  FROM = context.parameter[0].upper().strip()
+  TO = context.parameter[1].upper().strip()
+  try:
+    NB = NB if len(context.parameter) == 2 else float(context.parameter[2].strip())
+  except:
+    NB = 1.0
+  if currencies.count(FROM) == 0:
+    await context.edit(f"{FROM}{lang_rate['nc']}`{', '.join(currencies)}`")
+    return
+  if currencies.count(TO) == 0:
+    await context.edit(f"{TO}{lang_rate['nc']}{', '.join(currencies)}`")
+    return
+  endpoint = f"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/{FROM.lower()}/{TO.lower()}.json"
+  with urllib.request.urlopen(endpoint) as response:
+    result = response.read()
     try:
-        NB = float(context.parameter[2].strip())
-    except:
-        NB = 1.0
-    if currencies.count(FROM) == 0:
-        await context.edit(
-            f"{FROM}{lang_rate['nc']}{', '.join(currencies)}")
-        return
-    if currencies.count(TO) == 0:
-        await context.edit(f"{TO}{lang_rate['nc']}{', '.join(currencies)}")
-        return
-    rate_num = round(rates[TO] / rates[FROM] * NB, 2)
-    await context.edit(f'{FROM} : {TO} = {NB} : {rate_num}')
+      rate_data = json.loads(result)
+      await context.edit(f'`{FROM} : {TO} = {NB} : {round(NB * rate_data[TO.lower()], 4)}`\n\n{lang_rate["warning"]}')
+    except Exception as e:
+      await context.edit(str(e))
