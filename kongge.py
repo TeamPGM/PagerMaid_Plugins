0ee89ac7d48f30ac3f15bb41464eb8c410e5c5b7
xtaodada <xtao@xtaolink.cn>
2021-06-28 13:51:17 +0800

    kongge 停止维护

--- kongge.py
+++ kongge.py
@@ -10,11 +10,14 @@ except ImportError:
 
 @listener(is_plugin=True, outgoing=True, ignore_edited=True)
 async def kongge(context):
-    if jieba_imported and context.text and not context.via_bot and not context.forward:
-        if context.text.startswith('-') or context.text.startswith('/'):
-            return
-        seg_list = jieba.cut(context.text)
-        seg_txt = ' '.join(seg_list)
-        seg_txt.replace('@ ', '@')
-        if not seg_txt == context.text:
-            await context.edit(seg_txt)
+    try:
+        if jieba_imported and context.text and not context.via_bot and not context.forward:
+            if context.text.startswith('-') or context.text.startswith('/'):
+                return
+            seg_list = jieba.cut(context.text)
+            seg_txt = ' '.join(seg_list)
+            seg_txt.replace('@ ', '@')
+            if not seg_txt == context.text:
+                await context.edit(seg_txt)
+    except:
+        pass
