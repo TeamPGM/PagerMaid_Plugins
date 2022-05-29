561e8361247abbef69c947f1d66aaae0dc3cc097
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-05-29 11:59:46 +0800

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -1,9 +1,9 @@
 from pyrogram import Client
 from pagermaid.listener import listener
-from pagermaid.utils import Message, execute, alias_command
+from pagermaid.utils import Message, execute
 
 
-@listener(is_plugin=False, outgoing=True, command=alias_command("cal"),
+@listener(command="cal",
           description="计算\n示例：\n`-cal 1+1`加法\n`-cal 2-1`减法\n`-cal 1*2`乘法\n`-cal 4/2`除法\n`-cal 4^2`幂运算\n`-cal sqrt(4)`开方",
           parameters="<基本运算>")
 async def cal(_: Client, message: Message):
@@ -21,9 +21,9 @@ async def cal(_: Client, message: Message):
     else:
         return
 
-@listener(is_plugin=False, outgoing=True, command=alias_command("con"),
-            description="换算\n示例：\n`-con 2 99`将99转换为2进制",
-            parameters="<进制(数字)> <数值>")
+@listener(command="con",
+          description="换算\n示例：\n`-con 2 99`将99转换为2进制",
+          parameters="<进制(数字)> <数值>")
 async def con(_: Client, message: Message):
     command = message.arguments.split()
     if not command:
