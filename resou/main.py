a86abca926756ebe58bbb843a0157f7ee8a61632
zhxy-CN <admin@owo.cab>
2022-06-07 22:40:51 +0800

    Plugin resou adaptive fit Pagermaid_Pyro

---
+++ resou/main.py
@@ -0,0 +1,64 @@
+import json
+from requests import get
+from json.decoder import JSONDecodeError
+from pyrogram import Client
+from pagermaid.listener import listener
+from pagermaid.utils import Message
+
+
+@listener(command="zhrs",
+          description="知乎热搜。")
+async def zhrs(_: Client, context: Message):
+    await context.edit("获取中 . . .")
+    req = get("https://tenapi.cn/zhihuresou")
+    if req.status_code == 200:
+        try:
+            data = json.loads(req.text)
+        except JSONDecodeError:
+            await context.edit("出错了呜呜呜 ~ API 数据解析失败。")
+            return
+        res = '知乎实时热搜榜：\n'
+        for i in range(0, 10):
+            res += f'\n{i + 1}.「<a href={data["list"][i]["url"]}>{data["list"][i]["query"]}</a>」'
+        await context.edit(res)
+    else:
+        await context.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
+
+
+@listener(is_plugin=True, outgoing=True, command="wbrs",
+          description="微博热搜。")
+async def wbrs(_: Client, context: Message):
+    await context.edit("获取中 . . .")
+    req = get("https://tenapi.cn/resou")
+    if req.status_code == 200:
+        try:
+            data = json.loads(req.text)
+        except JSONDecodeError:
+            await context.edit("出错了呜呜呜 ~ API 数据解析失败。")
+            return
+        res = '微博实时热搜榜：\n'
+        for i in range(0, 10):
+            res += f'\n{i + 1}.「<a href={data["list"][i]["url"]}>{data["list"][i]["name"]}</a>」  ' \
+                   f'热度：{data["list"][i]["hot"]}'
+        await context.edit(res)
+    else:
+        await context.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
+
+
+@listener(is_plugin=True, outgoing=True, command="dyrs",
+          description="抖音热搜。")
+async def dyrs(_: Client, context: Message):
+    await context.edit("获取中 . . .")
+    req = get("https://tenapi.cn/douyinresou")
+    if req.status_code == 200:
+        try:
+            data = json.loads(req.text)
+        except JSONDecodeError:
+            await context.edit("出错了呜呜呜 ~ API 数据解析失败。")
+            return
+        res = '抖音实时热搜榜：\n'
+        for i in range(0, 10):
+            res += f'\n{i + 1}.「{data["list"][i]["name"]}」  热度：{data["list"][i]["hot"]}'
+        await context.edit(res)
+    else:
+        await context.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
