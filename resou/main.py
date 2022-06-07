9eed756f852a9f5a605f4d073ab658fd8c643414
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-06-07 22:49:16 +0800

    Update main.py

--- resou/main.py
+++ resou/main.py
@@ -1,64 +1,59 @@
-import json
-from requests import get
-from json.decoder import JSONDecodeError
 from pyrogram import Client
+from json.decoder import JSONDecodeError
 from pagermaid.listener import listener
-from pagermaid.utils import Message
+from pagermaid.utils import Message, client
 
 
 @listener(command="zhrs",
           description="知乎热搜。")
-async def zhrs(_: Client, context: Message):
-    await context.edit("获取中 . . .")
-    req = get("https://tenapi.cn/zhihuresou")
+async def zhrs(_: Client, message: Message):
+    req = await client.get("https://tenapi.cn/zhihuresou")
     if req.status_code == 200:
         try:
-            data = json.loads(req.text)
+            data = req.json()
         except JSONDecodeError:
-            await context.edit("出错了呜呜呜 ~ API 数据解析失败。")
+            await message.edit("出错了呜呜呜 ~ API 数据解析失败。")
             return
         res = '知乎实时热搜榜：\n'
         for i in range(0, 10):
             res += f'\n{i + 1}.「<a href={data["list"][i]["url"]}>{data["list"][i]["query"]}</a>」'
-        await context.edit(res)
+        await message.edit(res)
     else:
-        await context.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
+        await message.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
 
 
 @listener(command="wbrs",
           description="微博热搜。")
-async def wbrs(_: Client, context: Message):
-    await context.edit("获取中 . . .")
-    req = get("https://tenapi.cn/resou")
+async def wbrs(_: Client, message: Message):
+    req = await client.get("https://tenapi.cn/resou")
     if req.status_code == 200:
         try:
-            data = json.loads(req.text)
+            data = req.json()
         except JSONDecodeError:
-            await context.edit("出错了呜呜呜 ~ API 数据解析失败。")
+            await message.edit("出错了呜呜呜 ~ API 数据解析失败。")
             return
         res = '微博实时热搜榜：\n'
         for i in range(0, 10):
             res += f'\n{i + 1}.「<a href={data["list"][i]["url"]}>{data["list"][i]["name"]}</a>」  ' \
                    f'热度：{data["list"][i]["hot"]}'
-        await context.edit(res)
+        await message.edit(res)
     else:
-        await context.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
+        await message.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
 
 
 @listener(command="dyrs",
           description="抖音热搜。")
-async def dyrs(_: Client, context: Message):
-    await context.edit("获取中 . . .")
-    req = get("https://tenapi.cn/douyinresou")
+async def dyrs(_: Client, message: Message):
+    req = await client.get("https://tenapi.cn/douyinresou")
     if req.status_code == 200:
         try:
-            data = json.loads(req.text)
+            data = req.json()
         except JSONDecodeError:
-            await context.edit("出错了呜呜呜 ~ API 数据解析失败。")
+            await message.edit("出错了呜呜呜 ~ API 数据解析失败。")
             return
         res = '抖音实时热搜榜：\n'
         for i in range(0, 10):
             res += f'\n{i + 1}.「{data["list"][i]["name"]}」  热度：{data["list"][i]["hot"]}'
-        await context.edit(res)
+        await message.edit(res)
     else:
-        await context.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
+        await message.edit("出错了呜呜呜 ~ 无法访问到 API 服务器 。")
