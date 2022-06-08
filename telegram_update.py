5625abc8fb702aa0ff6a045809ab0581fca83d41
xtaodada <xtao@xtaolink.cn>
2022-06-08 17:03:43 +0800

    修复 Github Action

--- telegram_update.py
+++ telegram_update.py
@@ -1,10 +1,10 @@
 import json, time, sys
-from requests import get, post
+from httpx import get, post
 
 token = str(sys.argv[1])
-main = json.loads(get("https://api.github.com/repos/xtaodada/PagerMaid_Plugins/commits/master").content)
+main = json.loads(get("https://api.github.com/repos/TeamPGM/PagerMaid_Plugins/commits/master").content)
 text = "#更新日志 #" + main['commit']['author']['name'].replace('_', '') + \
-       ' \n\n🔨 [' + main['sha'][0:7] + '](https://github.com/Xtao-Labs/PagerMaid_Plugins/commit/' + \
+       ' \n\n🔨 [' + main['sha'][0:7] + '](https://github.com/TeamPGM/PagerMaid_Plugins/commit/' + \
        main['sha'] + '): ' + main['commit']['message']
 push_content = {'chat_id': '-1001441461877', 'disable_web_page_preview': 'True', 'parse_mode': 'markdown',
                 'text': text}
@@ -16,7 +16,7 @@ except:
 push_content['chat_id'] = '-1001319957857'
 time.sleep(1)
 try:
-    main_req = get(url, data=push_content)
+    main_req = post(url, data=push_content)
 except:
     pass
 print(main['sha'] + " ok！")
